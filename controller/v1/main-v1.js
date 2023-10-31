350169cc25c246c1fc93572c2855654dd264e7cb
phamkhoa <phamkhoa.dev1510@gmail.com>
2023-10-31 20:45:57 +0700

    31.10

---
+++ controller/v1/main-v1.js
@@ -0,0 +1,102 @@
+import { https } from "../../service/service.js";
+import { getDataFrom, renderShoeList, showDataFrom } from "./controller-v1.js";
+
+function fetchShoeList() {
+  https
+    .get("/shoe")
+    .then((res) => {
+      console.log("res.data", res.data);
+      renderShoeList(res.data.reverse());
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+fetchShoeList();
+function deleteShoe(id) {
+  https
+    .delete(`/shoe/${id}`)
+    .then((res) => {
+      fetchShoeList();
+      console.log("del tcong", res);
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
+window.deleteShoe = deleteShoe;
+window.addShoe = () => {
+  let shoe = getDataFrom();
+  https
+    .post("/shoe", shoe)
+    .then((res) => {
+      fetchShoeList();
+      $("#exampleModal").modal("hide");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// window.addShoe = (id) => {
+//   $("#exampleModal").modal("show");
+//   https
+//     .get(`/shoe/${id}`)
+//     .then((res) => {
+//       showDataFrom(res.data);
+//     })
+//     .catch((err) => {
+//       console.log("😃 - file: main-v1.js:50 - err:", err);
+//     });
+// };
+window.editShoe = (id) => {
+  $("#exampleModal").modal("show");
+  https
+    .get(`/shoe/${id}`)
+    .then((res) => {
+      showDataFrom(res.data);
+    })
+    .catch((err) => {
+      console.log("😃 - file: main-v1.js:50 - err:", err);
+    });
+};
+
+window.updateShoe = () => {
+  let shoe = getDataFrom();
+  https
+    .put(`/shoe/${shoe.ma}`, shoe)
+    .then((res) => {
+      fetchShoeList();
+      $("#exampleModal").modal("hide");
+    })
+    .catch((err) => {
+      console.log("😃 - file: main-v1.js:61 - err:", err);
+    });
+};
+///////////////////// giỏ hàng
+// bật chi tiết để thêm vào giỏ hàng
+window.ShowDetail = (id) => {
+  $("#exampleModal").modal("show");
+  https
+    .get(`/shoe/${id}`)
+    .then((res) => {
+      showDataFrom(res.data);
+    })
+    .catch((err) => {
+      console.log("😃 - file: main-v1.js:50 - err:", err);
+    });
+};
+// thêm vào giỏ hàng
+window.addShopShoe = () => {
+  let shopShoe = getDataFrom();
+
+  https
+    .post("shoeGH", shopShoe)
+    .then((res) => {
+      console.log("😃 - file: main-v1.js:45 - then - res:", res);
+      $("#exampleModal").modal("hide");
+      Swal.fire("Đã Thêm Vào Giỏ Hàng!", "You clicked the button!", "success");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
---
+++ service/service.js
@@ -0,0 +1,7 @@
+// axios instance
+export let https = axios.create({
+  baseURL: "https://653a19bde3b530c8d9e928d8.mockapi.io",
+});
+export let https_2 = axios.create({
+  baseURL: "https://653a19bde3b530c8d9e928d8.mockapi.io",
+});
